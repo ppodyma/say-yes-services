081bcbcead8ec3344fa8b7a0de4522e2661272ae
Piotr Podyma <piotr.podyma_ext@scout24.com>
2020-04-21 20:03:26 +0200

    Update AboutUs Section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
-import './Style.scss'
+import './Style.scss';
+import SVG from 'react-inlinesvg';
 
 function Main() {
   return (
@@ -8,16 +9,20 @@ function Main() {
      <div className="darkBackground"></div>
      <div className="mainText">
         <div>
-          Profesjonalizm | Doświadczenie | Szkolenia
+          {/* Profesjonalizm | Doświadczenie | Szkolenia */}
+          {/* Działamy niezawodnie  */}
         </div>
         <a href="#aboutSection">
           <Button variant="outlined" className="contactButton">Dowiedź się o nas więcej</Button>
         </a>
      </div>
      <div className="logo">
-         <img src={require("./../../assets/imgs/logo3.png")}  />
+         {/* <img src={require("./../../assets/imgs/logo.svg")}  /> */}
+         <SVG
+          src={require("./../../assets/imgs/logo.svg")}
+        />
          <br />
-         Say Yes Services
+         Napędzani pasją | Działamy niezawodnie
      </div>
     </div>
   );
